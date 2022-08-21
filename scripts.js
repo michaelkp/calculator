39ab791f1cbf5291acb61cadaaba75b634e3d772
Michael <michaelk99@gmail.com>
2022-08-21 14:59:18 -0700

    Add keyboard support

--- scripts.js
+++ scripts.js
@@ -20,33 +20,67 @@ let currentNumber = 0
 let signHasValue = false
 let operated = false
 let calculated = false
+
 const DISPLAY = document.querySelector('#display')
 DISPLAY.textContent = `${currentNumber}`
+
 const NUMBERS = document.querySelectorAll('.number')
-NUMBERS.forEach(num => {
+NUMBERS.forEach(num => getNumbers(num))
+window.addEventListener('keydown', (e) => {
+    const KB_NUM = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']
+    if(KB_NUM.includes(e.key)) {
+        console.log(e.key + ' key test')
+        values.push(e.key)
+        number = values.join('')
+        number = parseFloat(number)
+        getPrevNum(number)
+    } else if (!KB_NUM.includes(e.key)){
+        console.log('nan')
+        return
+    }
+})
+
+function getNumbers(num) {
     num.addEventListener('click', (e) => {
         values.push(num.value)
         number = values.join('')
         number = parseFloat(number)
         console.log(number + '--number test')
-        //getNumbers(number)
         getPrevNum(number)
     })
-})
+}
 
 const OPERANDS = document.querySelectorAll('.operand')
 OPERANDS.forEach(operand => {
     operand.addEventListener('click', () => {
+        sign = operand.value
+        getOperand(sign)
+    })
+})
+
+window.addEventListener('keydown', (e) => {
+    const KB_SIGN = ['+', '-', '*', '/']
+    if(KB_SIGN.includes(e.key)) {
+        sign = e.key
+        console.log(sign + '--kbsign test')
+        getOperand(sign)
+    } else if(!KB_SIGN.includes(e.key)){
+        return
+    }
+})
+
+function getOperand(sign, operand) {
+    
         if(operated === true){
             console.log(operated + '-- operated test')
             console.log(sign + '-- operated sign test')
             console.log(nextNum + '-- operated nextNum test')
             console.log(prevNum + '-- operated prevNum test')
-            sign = operand.value
+            //sign = operand.value
             DISPLAY.textContent = `${currentNumber} ${sign}`
             operate(sign, prevNum, nextNum)
         } else if(calculated === true){
-            sign = operand.value
+            //sign = operand.value
             console.log(currentNumber + 'currentNum' + sign + ' ' + calculated)
             nextNum = ''
             number = ''
@@ -54,7 +88,7 @@ OPERANDS.forEach(operand => {
             DISPLAY.textContent = `${currentNumber}`
             getNextNum(sign)
             } else {
-                sign = operand.value
+                //sign = operand.value
                 signHasValue = true
                 values.length = 0
                 number = ''
@@ -64,19 +98,34 @@ OPERANDS.forEach(operand => {
                 console.log(sign + '--getOperand sign test')
                 getNextNum(sign)
             }
-        })
-    })
+        
+}
 
 const EQUAL = document.querySelector('#equals')
-EQUAL.addEventListener('click', () => {
-        DISPLAY.textContent = `${currentNumber} ${nextNum}`
-        operate(sign, prevNum, nextNum)
-        
-    })
+EQUAL.addEventListener('click', equals)
+window.addEventListener('keydown', (e) => {
+    if(e.key === 'Enter' || e.key === '=') {
+        equals()
+    } else {
+        return
+    }
+})
+
+function equals() {
+    DISPLAY.textContent = `${currentNumber} ${nextNum}`
+    operate(sign, prevNum, nextNum)
+}
 
 const CLEAR = document.querySelector('#clear')
-CLEAR.addEventListener('click', () => {
-    console.log(values + '--clear before test')
+CLEAR.addEventListener('click', clearNumber)
+window.addEventListener('keydown', (e) => {
+    if(e.key === 'Backspace') {
+        clearNumber()
+    }
+})
+
+
+function clearNumber() {
     if(operated === true){
         console.log(nextNum + '--clear nextNum test')
         values.splice(-1)
@@ -94,10 +143,17 @@ CLEAR.addEventListener('click', () => {
         DISPLAY.textContent = `${prevNum}`
         return prevNum
     }
-})
+}
 
 const ALL_CLEAR = document.querySelector('#ac')
-ALL_CLEAR.addEventListener('click', () => {
+ALL_CLEAR.addEventListener('click', allClear)
+window.addEventListener('keydown', (e) => {
+    if(e.key === 'Escape') {
+        console.log(e.key + '-- all clear')
+        allClear()
+    }
+})
+function allClear(e) {
     calculated = false
     operated = false
     signHasValue = false
@@ -107,7 +163,7 @@ ALL_CLEAR.addEventListener('click', () => {
     nextNum = ''
     currentNumber = ''
     DISPLAY.textContent = 0
-})
+}
 
 function getPrevNum() {
     console.log(number + '--getPrevNum number test')
@@ -220,7 +276,13 @@ function operate(sign, prevNum, nextNum) {
             //return prevNum
         }    
     }
+    if(sign === '/' && prevNum === 0 || sign === '/' && nextNum === 0) {
+        console.log('divide by zero')
+        DISPLAY.textContent = 'ERROR'
+        return
+    }
     if(sign === '/') {
+        
         if(calculated === true){
             currentNumber = divide(currentNumber, nextNum)
             nextNum = ''
@@ -248,3 +310,4 @@ function operate(sign, prevNum, nextNum) {
         }    
     }
 }
+
